a815038787674f9a6b6f4106a33984ead7fa5b90
peter anyankpele <anyankpelepeter7@gmail.com>
2025-07-03 13:57:26 -0700

    added user data for EC2, ECR setup,and Iam role

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -44,19 +44,16 @@ resource "aws_instance" "application_instance_1" {
     Name = "application_instance_1"
   }
 
-
-  # user_data = <<-EOF
-  #   #!/bin/bash
-  #   # Use this for your user data (script from top to bottom)
-  #   # install httpd (Linux 2 version)
-  #   yum update -y
-  #   yum install -y httpd
-  #   systemctl start httpd
-  #   systemctl enable httpd
-  #   echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-  #   EOF
+  user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  yum install -y docker
+  service docker start
+  $(aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 257394496890.dkr.ecr.us-west-2.amazonaws.com)
+  docker pull 257394496890.dkr.ecr.us-west-2.amazonaws.com/my-app:latest
+  docker run -d -p 80:80 257394496890.dkr.ecr.us-west-2.amazonaws.com/my-app:latest
+  EOF
 }
-
 
 resource "aws_instance" "application_instance_2" {
   depends_on = [
